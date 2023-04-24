e3e75be3c6ec3a7d37eaafb3dd109d347cc31283
hewliyang <hewliyang@u.nus.edu>
2023-04-24 21:45:56 +0800

    feat: active page highlighting on the nav

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
     import { drawerStore } from "@skeletonlabs/skeleton";
-    
+    import { page } from "$app/stores";
+
     function drawerClose(): void {
         drawerStore.close();
     }
+
+    $: curPath = $page.url.pathname;
 
 </script>
 
 <nav class="list-nav p-4">
     <ul>
-        <li><a href="/" on:click={drawerClose}>Home</a></li>
-        <li><a href="/about" on:click={drawerClose}>About</a></li>
+        <li><a class="{curPath === '/' ? 'variant-filled-primary' : ''}" href='/' on:click={drawerClose} >Home</a></li>
+        <li><a class="{curPath === '/about' ? 'variant-filled-primary' : ''}" href="/about" on:click={drawerClose}>About</a></li>
     </ul>
 </nav>
