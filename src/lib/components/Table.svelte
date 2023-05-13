4fd67ec31b92b6594f14a0e02855c55dbb885340
hewliyang <hewliyang@u.nus.edu>
2023-05-13 23:46:10 +0800

    update function location to singapore

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -19,16 +19,16 @@
 
 
 <div
-	class="w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
+	class="w-full max-w-xl p-6 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
 >
-	<div class="flex items-center justify-between mb-4">
+	<div class="flex flex-col md:flex-row items-center justify-between mb-4">
 		<div class="space-y-1">
 			<h2 class="text-xl font-semibold">Recent Users</h2>
 			<p class="text-sm text-gray-500">
 				Fetched {users.length} users in {duration ? duration : 'unknown'} ms
 			</p>
 		</div>
-		<button class="btn variant-ringed-secondary" on:click={refreshPage}>Refresh Page</button>
+		<button class="btn variant-ringed-secondary mt-3 md:mt-0" on:click={refreshPage}>Refresh</button>
 	</div>
 	<div class="divide-y divide-gray-900/5">
 		{#each users as user (user.id)}
--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 
     import Table from '$lib/components/Table.svelte';
+	import { CodeBlock } from '@skeletonlabs/skeleton';
     export let data;
 
 </script>
 
-<Table users={data.users} duration={data.duration} />
+<div class="container h-full mx-auto flex flex-col justify-center items-center space-y-5">
+    <h3>Data fetched from <strong> Vercel Postgres </strong></h3>
+    <Table users={data.users} duration={data.duration} />
+
+    <h4>Schema</h4>
+    <CodeBlock language="sql" code={`CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        image VARCHAR(255),
+        "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);`} />
+
+    <h4> Raw </h4>
+    <CodeBlock language="json" code={ JSON.stringify(data.users, null, 2) } />
+</div>
